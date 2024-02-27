4a45599c5f880c685644fe886202bcaf50315ae2
geneic-niue <s.suzuki@vrst.co.jp>
2024-02-27 16:05:06 +0900

    5 step done

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Dancing_Script } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] })
+export const dansing_script = Dancing_Script({subsets: ['latin']})
